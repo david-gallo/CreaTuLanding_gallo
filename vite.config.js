bd7fd50cfc2687f9245e3806b1e1c8e16d7b3b27
“david <tuemail@example.comda>
2025-07-27 11:13:52 -0300

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CreaTuLanding_gallo/',
+  base: '/CreaTuLanding_gallo/', // <- importante
   plugins: [react()],
-});
+})
